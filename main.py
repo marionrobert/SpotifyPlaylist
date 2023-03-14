660791205293ad608358b41eaeb983bce9420ffa
Marion <mrobert148@gmail.com>
2023-03-14 12:37:10 +0100

    get all songs title from the web_data using beautifulsoup

--- main.py
+++ main.py
@@ -6,5 +6,14 @@ URL = f"https://www.billboard.com/charts/hot-100/{specialdate}/"
 
 response = requests.get(URL)
 # print(response.status_code)
-data = response.content
-# print(data)
+web_data = response.content
+# print(web_data)
+
+soup = BeautifulSoup(web_data, "html.parser")
+# print(soup.prettify())
+
+all_songs = soup.select("li #title-of-a-story")
+print(len(all_songs))
+
+all_song_titles = [song.getText() for song in all_songs]
+print(all_song_titles)
